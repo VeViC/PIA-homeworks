9b02f7f16c692c4562c0367683f1585f4ea55643
VeViC <45897975+VeViC@users.noreply.github.com>
2021-01-31 21:33:30 +0100

    Search bar added
    omogucena pretraga filmova

--- hw-4/home-admin.php
+++ hw-4/home-admin.php
@@ -1,12 +1,34 @@
+<?php
+    $con= new mysqli('localhost', 'root', '', 'filmovi') or die("Nemoguce povezivanje");
+?>
 <!DOCTYPE html>
 <html>
 <head>
-    <title> Login </title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+<link rel='stylesheet' type="text/css" href='home.css'>
+<div class="topnav">
+    <a class="active" href="home-admin.php">Home</a><a class="add" href="add-movie.php">Add Movie</a>
+    <form action="home-admin.php" method="POST" id='searchForm'>
+        <input type="text" name="search" id="searchBar" placeholder="Search..." maxlength="25" autocomplete="off"/><input type="submit" id="searchBtn" value="Go!"/>
+    </form>
+    
+    <a class="leave" href="login.php">Logout</a>
+</div>
 </head>
 <body>
-    <h1> ADMIN </h1>
-
+    <?php
+        if(!isset($_POST['search'])){
+            echo "";
+        }else if($_POST['search'] !== ''){
+            $search = mysqli_real_escape_string($con, $_POST['search']);
+            $query = "SELECT * FROM lista WHERE naslov LIKE '%$search%' ";
+            $results = mysqli_query($con, $query);
+            while($row = mysqli_fetch_array($results)){
+                $id = $row['id'];
+                $naslov = $row['naslov'];
+                $opis = $row['opis'];
+                echo '<h2><a href="' . $id . '.php">' . $naslov . '</a></h2><h3>' . $opis . '</h3><br />';
+            }
+        }
+    ?>
 </body>
-
 </html>
